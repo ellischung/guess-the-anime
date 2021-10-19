445d680a798ae4c9ab5859135ba591d948657b70
Ellis <ellischung98@gmail.com>
2021-10-19 00:11:20 -0400

    set up socket.io connection for app

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const socket = require("socket.io");
 const app = express();
 const cors = require("cors");
 
 app.use(cors());
+app.use(express.json());
 
-app.listen("3000", () => {
-    console.log("Server running on Port 3000...");
+const server = app.listen('3000', () => {
+    console.log('Server running on Port 3000...');
+});
+
+io = socket(server);
+
+io.on('connection', (socket)=> {
+    console.log(socket.id);
+
+    socket.on('join_room', (data) => {
+        socket.join(data);
+        console.log('USER JOINED ROOM: ' + data);
+    });
+
+    socket.on('disconnect', ()=> {
+        console.log('USER DISCONNECTED');
+    });
 });
