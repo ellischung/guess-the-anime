24656bdebdf4e6aabae758fd515af4ca835ecdd0
Ellis <ellischung98@gmail.com>
2021-10-28 19:30:02 -0400

    made changes to server because of socket io updates

--- server/index.js
+++ server/index.js
@@ -1,26 +1,50 @@
 const express = require("express");
-const socket = require("socket.io");
 const app = express();
+const http = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 app.use(cors());
-app.use(express.json());
 
-const server = app.listen("3002", () => {
-    console.log("Server running on Port 3002...");
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    }
 });
-
-io = socket(server);
 
 io.on("connection", (socket) => {
     console.log(socket.id);
 
-    socket.on("join_room", (data) => {
-        socket.join(data);
-        console.log("USER JOINED ROOM: " + data);
-    });
-
     socket.on("disconnect", () => {
-        console.log("USER DISCONNECTED");
+        console.log("User disconnected", socket.id);
     });
 });
+
+server.listen(3001, () => {
+    console.log("Server running on port 3001");
+});
+
+
+// app.use(express.json());
+
+// const server = app.listen("3002", () => {
+//     console.log("Server running on Port 3002...");
+// });
+
+// io = socket(server);
+
+// io.on("connection", (socket) => {
+//     console.log(socket.id);
+
+//     socket.on("join_room", (data) => {
+//         socket.join(data);
+//         console.log("USER JOINED ROOM: " + data);
+//     });
+
+//     socket.on("disconnect", () => {
+//         console.log("USER DISCONNECTED");
+//     });
+// });
